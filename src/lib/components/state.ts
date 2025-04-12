caa7d37841cf7bf6dc242da86d628781ad0bc149
Kenny <101839505+khui0@users.noreply.github.com>
2025-04-12 14:23:00 -0400

    feat: not working

--- src/lib/components/state.ts
+++ src/lib/components/state.ts
@@ -1,36 +1,60 @@
 import { browser } from "$app/environment";
-import { writable, type Writable } from "svelte/store";
+import { get, writable, type Writable } from "svelte/store";
 
 export interface GameState {
   balance: number;
   bunny: number;
   grass: number;
   flower: number;
+  tree: number;
 }
 
+const DEFAULT_STATE = {
+  balance: 0,
+  bunny: 1,
+  grass: 0,
+  flower: 0,
+  tree: 0,
+};
+
 export const gameState: Writable<GameState | null> = writable(null);
+
+export const moneyPerSecond: Writable<number> = writable(10);
 
 if (browser) {
   const storedState = localStorage.getItem("state");
   if (storedState !== null) {
-    gameState.set(JSON.parse(storedState));
+    gameState.set(Object.assign(DEFAULT_STATE, JSON.parse(storedState)));
   }
 }
 
 gameState.subscribe((value) => {
   if (!browser) return;
-  if (value === null) {
-    localStorage.removeItem("state");
-  } else {
-    localStorage.setItem("state", JSON.stringify(value));
+  try {
+    if (value === null) {
+      localStorage.removeItem("state");
+    } else {
+      moneyPerSecond.set(calculateRate());
+      localStorage.setItem("state", JSON.stringify(value));
+    }
+  } catch (e) {
+    console.error(e);
   }
 });
 
+export function calculateRate(): number {
+  const state = get(gameState);
+  if (state === null) return 0;
+
+  let rate = 10;
+  rate += state.grass * 5;
+  rate += state.flower * 15;
+  rate *= 1.1 * state.tree;
+
+  return rate;
+}
+
 export function newGame() {
-  gameState.set({
-    balance: 0,
-    bunny: 0,
-    grass: 0,
-    flower: 0,
-  });
+  gameState.set(DEFAULT_STATE);
+  moneyPerSecond.set(10);
 }
--- src/routes/(game)/+layout.svelte
+++ src/routes/(game)/+layout.svelte
@@ -1,14 +1,29 @@
 <script lang="ts">
   import { goto } from "$app/navigation";
   import Button from "$lib/components/button.svelte";
+  import Bunny from "$lib/components/game/bunny.svelte";
+  import Flower from "$lib/components/game/flower.svelte";
+  import Grass from "$lib/components/game/grass.svelte";
   import Shop from "$lib/components/game/shop.svelte";
-  import { gameState } from "$lib/components/state";
+  import Tree from "$lib/components/game/tree.svelte";
+  import { gameState, moneyPerSecond } from "$lib/components/state";
+  import { onMount } from "svelte";
   import MingcuteBack2Fill from "~icons/mingcute/back-2-fill";
   import MingcuteShoppingCart1Fill from "~icons/mingcute/shopping-cart-1-fill";
 
   let { children } = $props();
 
   let shop: Shop | null = $state(null);
+
+  onMount(() => {
+    setInterval(() => {
+      if ($gameState === null) return;
+      $gameState.balance = $gameState?.bunny * $moneyPerSecond;
+      console.log($gameState, $moneyPerSecond);
+    }, 1000);
+  });
+
+  $inspect($moneyPerSecond)
 </script>
 
 <div class="bg-base-300/70 grid h-full w-full grid-rows-[auto_1fr_auto] gap-2 p-4">
@@ -36,6 +51,32 @@
           shop?.show();
         }}><MingcuteShoppingCart1Fill /> Shop</Button
       >
+    </div>
+    <div class="flex flex-wrap items-center gap-4 overflow-auto">
+      <span class="text-accent-content flex items-center gap-1 text-3xl font-semibold">
+        <div class="drop-shadow-sm">
+          <Bunny size={40} />
+        </div>
+        <p>{$gameState?.bunny}</p>
+      </span>
+      <span class="text-accent-content flex items-center gap-1 text-3xl font-semibold">
+        <div class="drop-shadow-sm">
+          <Grass size={40} />
+        </div>
+        <p>{$gameState?.grass}</p>
+      </span>
+      <span class="text-accent-content flex items-center gap-1 text-3xl font-semibold">
+        <div class="drop-shadow-sm">
+          <Flower size={40} />
+        </div>
+        <p>{$gameState?.flower}</p>
+      </span>
+      <span class="text-accent-content flex items-center gap-1 text-3xl font-semibold">
+        <div class="drop-shadow-sm">
+          <Tree size={40} />
+        </div>
+        <p>{$gameState?.tree}</p>
+      </span>
     </div>
   </div>
 </div>
