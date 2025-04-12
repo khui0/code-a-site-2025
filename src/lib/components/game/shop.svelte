cca5e0565bf557553e362cb4fabf3850532429e4
Kenny <101839505+khui0@users.noreply.github.com>
2025-04-12 14:42:39 -0400

    feat: shop

--- src/lib/components/game/shop.svelte
+++ src/lib/components/game/shop.svelte
@@ -1,5 +1,10 @@
 <script lang="ts">
   import Button from "../button.svelte";
+  import { gameState } from "../state.svelte";
+  import Bunny from "./bunny.svelte";
+  import Flower from "./flower.svelte";
+  import Grass from "./grass.svelte";
+  import Tree from "./tree.svelte";
 
   let modal: HTMLDialogElement | null = $state(null);
 
@@ -18,6 +23,104 @@
       <h3 class="absolute top-1.5 left-0 z-[-1] brightness-90">Shop</h3>
       <h3 class="">Shop</h3>
     </span>
+    <ul class="flex w-full flex-col gap-4">
+      <!-- Grass -->
+      <li
+        class={{
+          "rounded-box bg-base-200 flex w-full items-center gap-1 p-3": true,
+          "pointer-events-none opacity-50": (gameState.current?.balance || 0) < 100,
+        }}
+      >
+        <Grass size={40} />
+        <p class="text-accent-content text-3xl font-semibold">{gameState.current?.grass}</p>
+        <div class="flex flex-1 items-center justify-end gap-2">
+          <p class="text-primary-content text-3xl font-semibold">$100</p>
+          <Button
+            type="accent"
+            onclick={() => {
+              if (gameState.current === null) return;
+              if (gameState.current.balance >= 100) {
+                gameState.current.balance -= 100;
+                gameState.current.grass++;
+              }
+            }}
+            >Purchase Grass
+          </Button>
+        </div>
+      </li>
+      <!-- Flower -->
+      <li
+        class={{
+          "rounded-box bg-base-200 flex w-full items-center gap-1 p-3": true,
+          "pointer-events-none opacity-50": (gameState.current?.balance || 0) < 100,
+        }}
+      >
+        <Flower size={40} />
+        <p class="text-accent-content text-3xl font-semibold">{gameState.current?.flower}</p>
+        <div class="flex flex-1 items-center justify-end gap-2">
+          <p class="text-primary-content text-3xl font-semibold">$250</p>
+          <Button
+            type="accent"
+            onclick={() => {
+              if (gameState.current === null) return;
+              if (gameState.current.balance >= 250) {
+                gameState.current.balance -= 250;
+                gameState.current.flower++;
+              }
+            }}
+            >Purchase Flower
+          </Button>
+        </div>
+      </li>
+      <!-- Tree -->
+      <li
+        class={{
+          "rounded-box bg-base-200 flex w-full items-center gap-1 p-3": true,
+          "pointer-events-none opacity-50": (gameState.current?.balance || 0) < 500,
+        }}
+      >
+        <Grass size={40} />
+        <p class="text-accent-content text-3xl font-semibold">{gameState.current?.tree}</p>
+        <div class="flex flex-1 items-center justify-end gap-2">
+          <p class="text-primary-content text-3xl font-semibold">$500</p>
+          <Button
+            type="accent"
+            onclick={() => {
+              if (gameState.current === null) return;
+              if (gameState.current.balance >= 500) {
+                gameState.current.balance -= 500;
+                gameState.current.tree++;
+              }
+            }}
+            >Purchase Tree
+          </Button>
+        </div>
+      </li>
+      <!-- Bunny -->
+      <li
+        class={{
+          "rounded-box bg-base-200 flex w-full items-center gap-1 p-3": true,
+          "pointer-events-none opacity-50": (gameState.current?.balance || 0) < 1000,
+        }}
+      >
+        <Bunny size={40} />
+        <p class="text-accent-content text-3xl font-semibold">{gameState.current?.bunny}</p>
+        <div class="flex flex-1 items-center justify-end gap-2">
+          <p class="text-primary-content text-3xl font-semibold">$1000</p>
+          <Button
+            type="accent"
+            onclick={() => {
+              if (gameState.current === null) return;
+              if (gameState.current.balance >= 100) {
+                gameState.current.balance -= 1000;
+                gameState.current.bunny++;
+              }
+            }}
+            >Purchase Bunny
+          </Button>
+        </div>
+      </li>
+    </ul>
     <Button onclick={close}>Close</Button>
   </div>
   <form method="dialog" class="modal-backdrop">
--- src/routes/(game)/play/+page.svelte
+++ src/routes/(game)/play/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import { invalidateAll } from "$app/navigation";
   import Bunny from "$lib/components/game/bunny.svelte";
   import Egg from "$lib/components/game/egg.svelte";
   import Flower from "$lib/components/game/flower.svelte";
@@ -8,56 +9,65 @@
   import { gameState } from "$lib/components/state.svelte";
   import Alea from "alea";
   import { createNoise2D } from "simplex-noise";
+  import { onMount } from "svelte";
 
   const prng = Alea("stony-brook");
 
   const noise2D = createNoise2D(prng);
+
+  let ready: boolean = $state(false);
+
+  onMount(() => {
+    ready = true;
+  });
 </script>
 
 <div class="relative h-full w-full bg-sky-200">
-  <!-- Trees -->
-  <div class="absolute inset-0">
-    {#each Array(gameState.current?.tree) as _, i}
-      <div
-        class="absolute bottom-35 -translate-x-1/2"
-        style="left: {noise2D(i * 1.3, i / 2) * 50 + 50}%;"
-      >
-        <Tree />
-      </div>
-    {/each}
-  </div>
-  <!-- Grass -->
-  <div class="bg-accent absolute bottom-0 h-40 w-full">
-    {#each Array(gameState.current?.grass) as _, i}
-      <div
-        class="absolute -translate-x-1/2 -translate-y-1/2"
-        style="left: {noise2D(i, i) * 50 + 50}%; bottom: {100 * Math.sin(i * 7) - 40}%"
-      >
-        <Grass />
-      </div>
-    {/each}
-  </div>
-  <!-- Flower -->
-  <div class="absolute bottom-0 h-40 w-full">
-    {#each Array(gameState.current?.flower) as _, i}
-      <div
-        class="absolute -translate-x-1/2 -translate-y-1/2"
-        style="left: {noise2D(i * 2, i - 15) * 50 + 50}%; bottom: {100 * Math.sin(i * 2) - 40}%"
-      >
-        <Flower />
-      </div>
-    {/each}
-  </div>
-  <!-- Bunny -->
-  <div class="absolute bottom-0 h-40 w-full">
-    {#each Array(gameState.current?.bunny) as _, i}
-      <Mover {i} bottom="{100 * Math.sin(i * 2) - 40}%">
-        <div class="hop" style="animation-delay: {i * 100}ms;">
-          <Bunny />
+  {#if ready}
+    <!-- Trees -->
+    <div class="absolute inset-0">
+      {#each Array(gameState.current?.tree) as _, i}
+        <div
+          class="absolute bottom-35 -translate-x-1/2"
+          style="left: {noise2D(i * 1.3, i / 2) * 50 + 50}%;"
+        >
+          <Tree />
         </div>
-      </Mover>
-    {/each}
-  </div>
+      {/each}
+    </div>
+    <!-- Grass -->
+    <div class="bg-accent absolute bottom-0 h-40 w-full">
+      {#each Array(gameState.current?.grass) as _, i}
+        <div
+          class="absolute -translate-x-1/2 -translate-y-1/2"
+          style="left: {noise2D(i, i) * 50 + 50}%; bottom: {100 * Math.sin(i * 7) - 40}%"
+        >
+          <Grass />
+        </div>
+      {/each}
+    </div>
+    <!-- Flower -->
+    <div class="absolute bottom-0 h-40 w-full">
+      {#each Array(gameState.current?.flower) as _, i}
+        <div
+          class="absolute -translate-x-1/2 -translate-y-1/2"
+          style="left: {noise2D(i * 2, i - 15) * 50 + 50}%; bottom: {100 * Math.sin(i * 2) - 40}%"
+        >
+          <Flower />
+        </div>
+      {/each}
+    </div>
+    <!-- Bunny -->
+    <div class="absolute bottom-0 h-40 w-full">
+      {#each Array(gameState.current?.bunny) as _, i}
+        <Mover {i} bottom="{100 * Math.sin(i * 2) - 40}%">
+          <div class="hop" style="animation-delay: {i * 100}ms;">
+            <Bunny />
+          </div>
+        </Mover>
+      {/each}
+    </div>
+  {/if}
 </div>
 
 <style>
